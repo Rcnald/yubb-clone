22afc5aff6966d9d01a59c01360532eae9d2e1b6
Rcnald <ronaldomjunior05@gmail.com>
2023-02-01 14:42:55 -0300

    footer complete

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,7 @@
 const menuHeader = document.getElementById('menuMobileHeader')
 const buttonHeader = document.getElementById('buttonMobileHeader')
 const buttonHeaderIcon = document.getElementById('buttonMobileHeaderIcon')
+const arrowsDown = document.querySelectorAll('#menuMobileHeader > li > i')
 
 const toogleMobileMenu = function() {
     for(let i = 0; i <= arguments.length; ++i){
@@ -11,4 +12,7 @@ const toogleMobileMenu = function() {
 buttonHeader.addEventListener('click',function(){
     toogleMobileMenu(menuHeader,'hidden','flex')
     toogleMobileMenu(buttonHeaderIcon,'fa-xmark','fa-bars','text-2xl','pl-.5px')
+    for (const arrow of arrowsDown) {
+        toogleMobileMenu(arrow,'fa-solid', 'fa-caret-down', 'text-xs')
+      }
 })
