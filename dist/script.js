f9f91a3123bfd5b5ccb60d40467c8914d2852541
Rcnald <ronaldomjunior05@gmail.com>
2023-02-06 21:39:34 -0300

    navbar process

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,38 @@
 const buttonHeader = document.getElementById('buttonMobileHeader')
-const navHeaderLinks = document.getElementsByClassName('nav-links')
 
+const navLinks = document.getElementsByClassName('nav-links')
 const navButtons = document.getElementsByClassName('nav-buttons')
+
+
+const addEventClick = function(link, buttons){
+    for(let j = 0; j < 7; ++j){
+        console.log('a')
+        
+        buttons[j].addEventListener('click', function(){
+            let choosen = buttons[j]
+
+            for(let i = 0; i < navButtons.length; ++i){
+                navButtons[i].classList.remove('button-click')
+            }
+            choosen.classList.add('button-click')
+        })
+
+        if(j > 3){
+            console.log('b')
+            continue
+        }else{
+            link[j].addEventListener('click', function(){
+                let choosen = link[j]
+    
+                for(let i = 0; i < navLinks.length; ++i){
+                    navLinks[i].classList.remove('text-blue-link')
+                }
+                choosen.classList.add('text-blue-link')
+            })
+        }
+        
+    }
+}
 
 const toogleMobileMenu = function() {
     for(let i = 0; i <= arguments.length; ++i){
@@ -16,3 +47,5 @@ buttonHeader.addEventListener('click',function(){
     toogleMobileMenu(menuHeader,'hidden','flex')
     toogleMobileMenu(buttonHeaderIcon,'fa-xmark','fa-bars','text-2xl','pl-.5px')
 })
+
+addEventClick(navLinks, navButtons)
