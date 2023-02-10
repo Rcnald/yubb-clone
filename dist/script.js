e27aab8d63a111efce0e8ff46acb3fb8233a770e
Rcnald <ronaldomjunior05@gmail.com>
2023-02-10 17:05:30 -0300

    section progress

--- dist/script.js
+++ dist/script.js
@@ -1,38 +1,20 @@
 const buttonHeader = document.getElementById('buttonMobileHeader')
+const navLinks = Array.from(document.getElementsByClassName('nav-links'))
+const navButtons = Array.from(document.getElementsByClassName('nav-buttons'))
 
-const navLinks = document.getElementsByClassName('nav-links')
-const navButtons = document.getElementsByClassName('nav-buttons')
-
-
-const addEventClick = function(link, buttons){
-    for(let j = 0; j < 7; ++j){
-        buttons[j].addEventListener('click', function(){
-            let choosen = buttons[j]
-
-            for(let i = 0; i < navButtons.length; ++i){
-                navButtons[i].classList.remove('button-click')
-            }
-            choosen.classList.add('button-click')
-        })
-
-        if(j > 3){
-            continue
-        }else{
-            link[j].addEventListener('click', function(){
-                let choosen = link[j]
-    
-                for(let i = 0; i < navLinks.length; ++i){
-                    navLinks[i].classList.remove('text-blue-link')
-                }
-                choosen.classList.add('text-blue-link')
+const addEventClick = function(element, style){
+    element.forEach(function(el){
+        el.addEventListener('click', function(){
+            element.forEach(function(el){
+                el.classList.remove(style)
             })
-        }
-        
-    }
+            el.classList.add(style)
+        })
+    })
 }
 
 const toogleMobileMenu = function() {
-    for(let i = 0; i <= arguments.length; ++i){
+    for(let i = 0; i <= arguments.length; i++){
         arguments[0].classList.toggle(arguments[i + 1]);
     }
 }
@@ -45,4 +27,5 @@ buttonHeader.addEventListener('click',function(){
     toogleMobileMenu(buttonHeaderIcon,'fa-xmark','fa-bars','text-2xl','pl-.5px')
 })
 
-addEventClick(navLinks, navButtons)
+addEventClick(navLinks, 'text-blue-link')
+addEventClick(navButtons, 'button-click')
